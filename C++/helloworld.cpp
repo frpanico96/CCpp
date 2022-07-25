3b71ae12b6fd31a6cb91a731c7fc799fe652dee8
francescopanico <frpanico96@gmail.com>
2022-07-25 22:59:59 +0200

    [CPP] First Commit 2.0

---
+++ C++/helloworld.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <compare>
+using namespace std;
+
+
+int main() 
+{
+  // Comments are used to explain the code
+  cout << "value: " << 3 << endl;
+  /* This line of code is an example about printing on multiple lines */
+  cout << "A string" << endl << "Another string" << endl << "Still a string" << endl;
+
+  return 0;
+}
