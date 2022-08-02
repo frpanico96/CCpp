8cb513cbbddb4d6df5cae42bf5898a2991ed00a8
francescopanico <frpanico96@gmail.com>
2022-08-02 22:37:51 +0200

    [GDL] cpp learning

--- C++/sampleProgram.cpp
+++ C++/sampleProgram.cpp
@@ -55,6 +55,26 @@ int main(int argc, char **argv)
   cin >> age1;
   cout << "Hello " << fullName << "! You are " << age1 << " years old" << endl;
 
+  /* Working with different number systems */
+  int number1 = 15; // Decimal
+  int number2 = 017; // Octal
+  int number3 = 0x0f; // Hexadecimal
+  int number4 = 0b00001111; // Binary
+    // Numbers will be printed in decimal
+  cout << "Number 1 is: " << number1 << endl;
+  cout << "Number 2 is: " << number2 << endl;
+  cout << "Number 3 is: " << number3 << endl;
+  cout << "Number 4 is: " << number4 << endl; 
+
+  /* Integers */
+  cout << sizeof(int) << endl; // this will return 4 which is the bytes size occupied by int data type
+  int elephant_count; // This will initialize to random number
+  int lion_count {}; // Variable braced initialization, this will initialize to 0
+  int dog_count = 10; // Assignment variable initialization
+  int cat_count (15); // Functional variable initialization
+  int domestic_animals {dog_count + cat_count};
+  //int narrowing_conversion {2.9}; --> This throws a compiler error
+  //int narrowing_conversion_functional(2.9); --> This throws a compiler warning, but generates a data loss since the value stored will be 2 instead of 2.9
   return 0;
 }
 /* Function declaration */
