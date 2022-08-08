00bc74ad213b4e37ae8df4a2e9c4cf58e6d931bd
francescopanico <frpanico96@gmail.com>
2022-08-08 23:52:03 +0200

    [GDL] cpp learning

---
+++ C++/data_operations.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+/* Operations on Data Chapter*/
+
+int main()
+{
+  /* 1 - Basic Operations */
+  int num1 {2};
+  int num2 {7};
+
+  cout << "Addition is: " << num1 + num2 << endl; // --> Addition
+  cout << "Subtraction is: " << num2 - num1 << endl; // --> Subtraction
+  cout << "Multiplication is: " << num1 * num2 << endl; // --> Multiplication
+  cout << "Division is: " << num2/num1 << " With a reminder of: " << num2%num1 << endl; // --> Division and Modulo operation
+
+  /* 2 - Precedence and Associativity
+}
--- C++/data_types.cpp
+++ C++/data_types.cpp
@@ -4,6 +4,8 @@ using namespace std;
 
 int main()
 {
+
+  /* 1 - Integers */
   unsigned short unsigned_short {456};
   signed short signed_short {-456};
 
@@ -29,6 +31,8 @@ int main()
 
   cout << "unsigned long long: "<< sizeof(unsigned_long_long) << endl;
   cout << "signed long long: "<< sizeof(signed_long_long) << endl;
+
+  /* 2 - Floating Numbers */
 
   float float_number {1.12345678901234567890f};
   double double_number {1.12345678901234567890};
@@ -68,4 +72,70 @@ int main()
   result = number10/number11;
   cout << number10 << " / " << number11 << " = " << result << endl;
 
+  /* 3 - Booleans */
+
+  bool red_light {true};
+  bool green_light {false};
+
+  if(red_light == true)
+  {
+    cout << "Stop!" << endl;
+  }
+  else
+  {
+    cout << "Go Thorugh!" << endl;
+  }
+  if(green_light)
+  {
+    cout << "The light is green" << endl;
+  }
+  else
+  {
+    cout << "The Light is Not Green" << endl;
+  }
+
+  cout << sizeof(bool) << endl; // Check the size of booleans
+  cout << "Red Light: " << red_light << endl; // --> Booleans are printed as numbers True: 1
+  cout << "Green Light: " << green_light << endl; // --> Booleans are printed as numbers False: 0
+  cout << boolalpha; // --> This statement will make boolean be printed as true or false rather then 1 or 0
+  cout << "Red Light is " << red_light << endl;
+  cout << "Green Light is " << green_light << endl;
+
+  /* 4 - Characters and Text */
+  char character1 {'a'};
+
+  cout << sizeof(char) << endl; // --> Char occupies 1 byte
+
+  char character2 = 65; // --> ASCII encoding
+  cout << "Character2 as char is " << character2 << endl; //--> Prints the corrisponding Character wich is "A"
+  cout << "Charcater2 as int is " << static_cast<int>(character2) << endl; // --> Prints the int value which is 65
+
+  /* 5 - Auto keyword */
+
+  auto var1 {12}; // --> Inteprets as integer
+  auto var2 {13.0}; // --> Inteprets as double
+  auto var3 {14.0f}; // --> Inteprets as float
+  auto var4 {15.0l}; // --> Inteprets as long double 
+  auto var5 {'e'}; // --> Inteprets as character
+  auto var6 {true}; // --> Inteprets as boolean
+
+  cout << "var1 occupies: " << sizeof(var1) << endl;
+  cout << "var2 occupies: " << sizeof(var2) << endl;
+  cout << "var3 occupies: " << sizeof(var3) << endl;
+  cout << "var4 occupies: " << sizeof(var4) << endl;
+  cout << "var5 occupies: " << sizeof(var5) << endl;
+  cout << "var6 occupies: " << sizeof(var6) << endl;
+
+  /* 6 - Assignments */
+  int var7 {123}; // --> Declarationa and Intialization
+  cout << "var7 is: " << var7 << endl;
+  var7 = 65; // --> Assignment
+  cout << "var7 now is: " << var7 << endl;
+
+  auto var8 {333u}; // --> Declaring and initializing an unsigned int
+  var8 = -22; //--> Assigning a negative value does not returns a compiler error and assign a random value
+
+  cout << "Var 8 should be 333 and is instead: " << var8 << endl;
+
+  return 0;
 }
