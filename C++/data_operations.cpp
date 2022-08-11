15af31dcb3f26249800c36550f68591f5541e001
francescopanico <frpanico96@gmail.com>
2022-08-11 21:30:18 +0200

    [GDL] cpp learning

--- C++/data_operations.cpp
+++ C++/data_operations.cpp
@@ -15,4 +15,20 @@ int main()
   cout << "Division is: " << num2/num1 << " With a reminder of: " << num2%num1 << endl; // --> Division and Modulo operation
 
   /* 2 - Precedence and Associativity*/
+  int a {6};
+  int b {3};
+  int c {8};
+  int d {9};
+  int e {3};
+  int f {2};
+  int g {5};
+
+  int result2 = a + b * c - d/e - f + g; // --> Multiplications and Divisions are evaluated before Additions and Subtractions
+  cout << "result: " << result2 << endl;
+  result2 = a/b*c +d - e +f; //-->Following associativity rules operations are performed left to right with precedence to Multiplications and Divisions
+  cout << "result: " << result2 << endl;
+  result2 = a/(b*c) +d - e +f; // --> Parenthesis override Precedence and Associativity rules
+  cout << "result: " << result2 << endl;
+
+
 }
